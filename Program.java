6e21fb008850fd5598d076038ff09f35ab07df5d
zkarakasis <zkarakasis@york.citycollege.eu>
2026-03-30 21:11:04 +0300

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,18 @@
+public class Program{
+    
+    public static void main (String[]args ){
+        Student s1 = new Student;
+        Rectangle r1 = new Rectangle;
+
+        System.out.println("Initial height: "+r1.getHeight());
+        System.out.println("Initial width: "+r1.getWidth());
+
+        r1.setHeight(5.0);
+        r1.setWidth(7.0);
+
+        System.out.println("New height: "+r1.getHeight());
+        System.out.println("New width: "+r1.getWidth());
+    }
+
+    
+}
